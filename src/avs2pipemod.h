61039fa4e182e6d76288094fb562ee824d454422
Cweb <cwebdesign@users.noreply.github.com>
2025-03-23 21:15:22 +0100

    PVS Studio headers added

--- src/avs2pipemod.h
+++ src/avs2pipemod.h
@@ -1,3 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 /*
  * Copyright (C) 2012-2016 Oka Motofumi <chikuzen.mo at gmail dot com>
  *
